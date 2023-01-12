5379cd153cb9de5ad577d78ba3e7dd81e56d76a3
levent <lvnt4834celebi@gmail.com>
2023-01-12 23:46:18 +0300

    margin

--- app.js
+++ app.js
@@ -1,45 +1,5 @@
-//  const firstp = document.querySelector("p")
-// console.log(firstp);
 
-// let p1 = document.getElementById("p1")
 
-// let allp = document.querySelectorAll("p")
-
-// allp.forEach(x=> {
-//   x.style.backgroundColor="blue"
-//   x.style.color="white"
-//   x.style.border = "2px solid red"
-//   x.style.width ="200px"
-//   x.style.fontSize ="2rem"
-
-// })
-
-// let div = document.getElementsByTagName("div")
-// console.log(div);
-
-// div[0].style.display = "flex"
-// div[0].style.justifyContent = "space-around"
-
-// let tekler = document.querySelectorAll("p:nth-child(odd)")
-
-// console.log(tekler);
-
-// let çift = document.querySelectorAll("p:nth-child(even)")
-
-// console.log(çift);
-
-// tekler.forEach(x=> x.style.backgroundColor="red")
-// çift.forEach(x=> x.style.backgroundColor="black")
-
-// allp.forEach(x=>x.classList.add("headertitle"))
-
-// allp.forEach(x=>x.classList = "class1 class2 class3")
-
-// console.log(allp);
-
-// allp.addEventListener("keydown",()=>{
-//   console.log(allp.textContent="hello");
-// })
 let par1 = document.getElementById("par1");
 let par2 = document.getElementById("par2");
 let body = document.getElementById("bod");
@@ -66,7 +26,7 @@ par1.style.backgroundColor = "gray";
 par2.style.backgroundColor = "gray";
 
 let div = document.getElementsByTagName("div");
-div[0].style.margin = "40% ";
+
 let pa = document.querySelectorAll("p");
 
 pa.forEach((x) => (x.style.textAlign = "center"));
